932e6dd7605aa0b7425d52d8ced5dac1d1150ba0
chope <Chopetrumpis@gmail.com>
2022-06-08 21:58:48 -0300

    Added and removed things.
    Added personal info
    Removed github button
    Now Projects button is used for my github repositories.
    Added My skills.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { AiFillGithub } from "react-icons/ai";
 
 function App() {
   return (
     <div className="App"> 
     <Navbar></Navbar>
       <header className="App-header">
+        <div className='info'>
+          <h2 className='by'>Website created by</h2>
+          <h1 className='name'>Sebastian "Chope" Trumpis</h1>
+          <h2 className='work'>Student developer</h2>
+        </div>
         <header className='About-me-header'>
           <h1 className="About-me-title">Who am i?</h1>
           <p className="About-me">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor feugiat dictum. Pellentesque non lacus arcu. Aliquam aliquam viverra rutrum. Phasellus molestie pellentesque odio at auctor. Proin a eros neque. Nunc egestas libero eget vestibulum condimentum. Mauris tristique iaculis neque, a blandit diam cursus eget. Praesent eget commodo velit, eget laoreet ante. Nullam a nisl nec nisi tempus cursus eu iaculis eros. Mauris eu turpis pellentesque, molestie ex id, interdum magna. In dui turpis, feugiat et rutrum sit amet, laoreet vitae sem. Aliquam erat volutpat. Donec in sapien non lectus facilisis accumsan. In blandit eros quis sagittis molestie. Mauris fringilla dolor vel iaculis accumsan. Maecenas egestas leo felis, et egestas purus faucibus vel.</p>
         </header>
-        <code className="bottom-text">chope's portfolio     Created in React
-        </code>
+        <header className='My-Skills-header'>
+          <h1 className='My-skills-title'>My Skills</h1>
+          <p className='My-skills'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor feugiat dictum. Pellentesque non lacus arcu. Aliquam aliquam viverra rutrum. Phasellus molestie pellentesque odio at auctor. Proin a eros neque. Nunc egestas libero eget vestibulum condimentum. Mauris tristique iaculis neque, a blandit diam cursus eget. Praesent eget commodo velit, eget laoreet ante. Nullam a nisl nec nisi tempus cursus eu iaculis eros. Mauris eu turpis pellentesque, molestie ex id, interdum magna. In dui turpis, feugiat et rutrum sit amet, laoreet vitae sem. Aliquam erat volutpat. Donec in sapien non lectus facilisis accumsan. In blandit eros quis sagittis molestie. Mauris fringilla dolor vel iaculis accumsan. Maecenas egestas leo felis, et egestas purus faucibus vel.</p>
+        </header>
       </header>
     </div>
   );
@@ -21,19 +27,16 @@ function App() {
       <nav className='navbar'>
         <ul className='navbar-nav'>
         <header className='nav-buttons'>
-          <a href='https://github.com/Chope2236' rel="noreferrer" target="_blank">
-            <button className='github' title='My Github'>Github</button>
-          </a>
-          <a href='#' rel="noreferrer" target="_blank">
+          <a href='localhost:3000' rel="noreferrer" target="_blank">
             <button className='linkedin' title='My LinkedIn'>LinkedIn</button>
           </a>
-          <a href='#' rel="noreferrer" target="_blank">
+          <a href='https://github.com/Chope2236?tab=repositories' rel="noreferrer" target="_blank">
             <button className='projects' title='Personal projects'>Projects</button>
           </a>
-          <a href='#' rel="noreferrer" target="_blank">
+          <a href='localhost:3000' rel="noreferrer" target="_blank">
             <button className='contact' title='Contact me'>Contact</button>
           </a>
-          <a href='#'>
+          <a href='localhost:3000'>
             <button className='home' title='Home page'>Home</button>
           </a>
         </header>
